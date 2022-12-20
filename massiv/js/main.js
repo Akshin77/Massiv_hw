8a905fd853ed037f8e2c4d4dbd29a2f45a660b2d
Akshin Ismailov <115021049+Akshin77@users.noreply.github.com>
2022-12-20 03:34:56 -0800

    first commit

---
+++ massiv/js/main.js
@@ -0,0 +1,10 @@
+
+
+let myname = [
+    ["Aston Martin","mersedes","chevrolet"],
+    ["hundai","bentley","kia"]
+]
+
+document.write("My name is "+myname[0][0][0]+myname[1][2][0]+myname[0][1][3]+myname[1][0][0]+myname[1][0][5]+myname[1][1][2])
+
+document.getElementById("myt").innerHTML = myname[0][0][0]+myname[1][2][0]+myname[0][1][3]+myname[1][0][0]+myname[1][0][5]+myname[1][1][2]
